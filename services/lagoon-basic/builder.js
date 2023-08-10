18b76e2e67aac17d401f2e32f6acad02b04ff4f1
cgoodwin90 <chris_2190@hotmail.com>
2023-08-10 17:22:24 +1000

    Initial setup for the basic type

---
+++ services/lagoon-basic/builder.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Modules
+const _ = require('lodash');
+
+// Builder
+module.exports = {
+  name: 'lagoon-basic',
+  config: {
+    version: 'custom',
+    confSrc: __dirname,
+    command: '',
+    port: '3000',
+    moreHttpPorts: [],
+  },
+  parent: '_lagoon',
+  builder: (parent, config) => class LandoLagoonBasic extends parent {
+    constructor(id, options = {}, factory) {
+      options = _.merge({}, config, options);
+
+      // Build basic
+      const basic = {
+        command: `/sbin/tini -- /lagoon/entrypoints.sh ${options.command}`,
+        ports: [options.port],
+      };
+      options.moreHttpPorts.push(options.port);
+      // Add in the basic service and push downstream
+      super(id, options, {services: _.set({}, options.name, basic)});
+    };
+  },
+};
